4fe669e5f5d8c6d643c83a5cc0877bfe71a51558
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2017-03-07 15:02:01 +0100

    [+] Simple project (will be removed later)

---
+++ src/main/java/org/java/fun/App.java
@@ -0,0 +1,18 @@
+/**
+ * Created by krzys on 2/28/2017.
+ */
+package org.java.fun;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+        System.out.println("Hi Travis!");
+    }
+
+    public static boolean isTrue()
+    {
+        return true;
+    }
+}
---
+++ src/test/java/org/java/fun/AssertTest.java
@@ -0,0 +1,16 @@
+package org.java.fun;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by krzys on 2/28/2017.
+ */
+public class AssertTest
+{
+    @Test
+    public void AssertionTest()
+    {
+        assertTrue(App.isTrue());
+    }
+}
